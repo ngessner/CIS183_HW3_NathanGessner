7402e22108bd6be81d6ad0b0aa5b9b84d9e83e08
pankeDev <ngessner04@gmail.com>
2024-10-21 23:16:59 -0400

    completed another activity

--- CIS183_StudentRegistration_NG/app/src/main/java/com/example/cis183_studentregistration_ng/MainActivity.java
+++ CIS183_StudentRegistration_NG/app/src/main/java/com/example/cis183_studentregistration_ng/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.cis183_studentregistration_ng;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button ma_btn_j_addNewStudent;
+    Intent intent_j_addNewStudent;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ma_btn_j_addNewStudent = findViewById(R.id.ma_btn_v_addNewStudent);
+        intent_j_addNewStudent = new Intent(MainActivity.this, activity_add_student.class);
+
+        addNewStudentButtonListener();
+    }
+
+    private void addNewStudentButtonListener()
+    {
+        ma_btn_j_addNewStudent.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(intent_j_addNewStudent);
+            }
         });
     }
 }
--- CIS183_StudentRegistration_NG/app/src/main/java/com/example/cis183_studentregistration_ng/activity_add_student.java
+++ CIS183_StudentRegistration_NG/app/src/main/java/com/example/cis183_studentregistration_ng/activity_add_student.java
@@ -1,15 +1,17 @@
 package com.example.cis183_studentregistration_ng;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class activity_add_student extends AppCompatActivity
 {
+    Button as_btn_j_back;
+    Intent intent_j_mainmenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,5 +20,21 @@ public class activity_add_student extends AppCompatActivity
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_student);
 
+        as_btn_j_back = findViewById(R.id.as_btn_v_back);
+        intent_j_mainmenu = new Intent(activity_add_student.this, MainActivity.class);
+
+        backButtonListener();
+    }
+
+    private void backButtonListener()
+    {
+        as_btn_j_back.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(intent_j_mainmenu);
+            }
+        });
     }
 }
